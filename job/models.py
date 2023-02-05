fb5bca3d233a94405ab8f6e918d600a4eecef03e
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-02-06 01:17:37 +0200

    create job models

--- job/models.py
+++ job/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+JOB_TYPE = [
+    ('FT', 'Full Time'),
+    ('PT', 'Part Time'),
+    ('RM', 'Remote')
+]
+
+class Job(models.Model):
+    title = models.CharField(max_length=100)
+    # location = 
+    job_type = models.CharField(max_length=20, choices=JOB_TYPE)
+    description = models.TextField(max_length=1000)
+    published_at = models.DateTimeField(auto_now=True)
+    vacancy = models.IntegerField(default=1)
+    salary = models.IntegerField(default=0)
+    # category =
+    experience = models.IntegerField(default=1)
+
+    def __str__(self) -> str:
+        return self.title
+
+
--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Created Apps
+    'job', 
+    
 ]
 
 MIDDLEWARE = [
