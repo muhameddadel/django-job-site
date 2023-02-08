e85da97011cd74cf8712abf0273ea0917dc28bba
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-02-08 03:20:07 +0200

    dealing with static and media

--- job/models.py
+++ job/models.py
@@ -7,6 +7,11 @@ JOB_TYPE = [
     ('RM', 'Remote')
 ]
 
+def image_upload(instace , file_name):
+    image_name , extension = file_name.split(".")
+    return 'jobs/%s.%s'%(instace.id, extension)
+
+
 class Job(models.Model):
     title = models.CharField(max_length=100)
     # location = 
@@ -17,6 +22,7 @@ class Job(models.Model):
     salary = models.IntegerField(default=0)
     category = models.ForeignKey('Category', on_delete= models.CASCADE)
     experience = models.IntegerField(default=1)
+    image = models.ImageField(upload_to=image_upload)
 
     def __str__(self) -> str:
         return self.title
--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,6 +122,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
