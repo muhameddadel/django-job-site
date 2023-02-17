e0a198d33963317e11c87208277841f321293986
muhameddadel <102751203+muhameddadel@users.noreply.github.com>
2023-02-17 02:37:57 +0200

    profile view

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import SignupForm
 from django.contrib.auth import authenticate, login
+from .models import Profile
 # Create your views here.
 
 def signup(request):
@@ -17,3 +18,13 @@ def signup(request):
         form = SignupForm()
 
     return render(request, 'registration/signup.html', {'form': form})
+
+
+def profile(request):
+    profile = Profile.objects.get(user = request.user)
+    return render(request, 'accounts/profile.html', {'profile': profile})
+
+
+
+def profile_edit(request):
+    return render(request, 'accounts/profile_edit.html', {})
